621cca6f0a1fb068989539156e3c978558a6db51
billy <billy_108@163.com>
2016-07-22 15:51:31 +0800

    scaleAnimation

--- app/src/main/java/com/zhaoshenghua/testcustomview/MainActivity.java
+++ app/src/main/java/com/zhaoshenghua/testcustomview/MainActivity.java
@@ -2,18 +2,31 @@ package com.zhaoshenghua.testcustomview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.LinearLayout;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private LinearLayout scoreView;
+    private Button btn;
+    private Animation animation;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        scoreView = (LinearLayout) findViewById(R.id.score_View);
-        scoreView.addView(new ScoreView(this, 80));
+        animation = AnimationUtils.loadAnimation(this, R.anim.scaleanim);
+        tv = (TextView) findViewById(R.id.tv);
+        btn = (Button) findViewById(R.id.btn_animation);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.startAnimation(animation);
+            }
+        });
     }
 
 }
